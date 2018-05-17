ee0d69588d213272f52a8e9ef98260ec1a273853
moshe <moswnd@gmail.com>
2018-05-17 22:23:03 +0300

    test

--- src/main/java/com/hit/server/Server.java
+++ src/main/java/com/hit/server/Server.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 import java.util.Observer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
+///T??E?st
 public class Server  extends Object implements Observer {
 
     private ServerSocket serversocket;
